b213552b631049ec18bb0fc29a589f0b3e1a4672
jelman <jelman523@gmail.com>
2023-12-20 09:36:54 -0800

    updated bids conversion config files and scripts

--- bids_conversion/1_wave2_bidsconversion.sh
+++ bids_conversion/1_wave2_bidsconversion.sh
@@ -6,12 +6,12 @@
  # Activate dcm2bids environment 
 #conda activate dcm2bids
 
-for vetsaid in `cat sublist_v2.txt`
+for vetsaid in `cat redofmrisublist.txt`
 do
 	echo "Starting conversion of ${vetsaid}..."
 
 	#run dcm2bids.
-	dcm2bids -d ~/netshare/SYNVETSACOPY/data/vetsa/VETSA2/orig/${vetsaid}_v2/ -p ${vetsaid} -s ses-02 -c ~/netshare/M/MRI/BIDS/code/dcm2bids_config_v2.json -o ~/netshare/M/MRI/BIDS/data/
+	dcm2bids -d ~/netshare/SYNVETSACOPY/data/vetsa/VETSA2/orig/${vetsaid}_v2/ -p ${vetsaid} -s ses-02 -c ~/netshare/M/MRI/BIDS/code/bids_conversion/dcm2bids_config_v2_fmri_ONLY.json -o ~/netshare/M/MRI/BIDS/data/
 	
 	echo "Finished converting ${vetsaid}."
 done
--- bids_conversion/1_wave3_bidsconversion.sh
+++ bids_conversion/1_wave3_bidsconversion.sh
@@ -8,12 +8,12 @@
  # Activate dcm2bids environment 
 conda activate dcm2bids
 
-for vetsaid in `cat sublist_v3.txt`
+for vetsaid in `cat v3_redo.txt`
 do
 	echo "Starting conversion of ${vetsaid}..."
 
 	#run dcm2bids.
-	dcm2bids -d ~/netshare/SYNVETSACOPY/data/vetsa/VETSA3_orig/orig/${vetsaid}_v3/ -p ${vetsaid} -s ses-03 -c ~/netshare/M/MRI/BIDS/code/dcm2bids_config_v3_diffusionONLY.json -o ~/netshare/M/MRI/BIDS/data/
+	dcm2bids -d ~/netshare/SYNVETSACOPY/data/vetsa/VETSA3_orig/orig/${vetsaid}_v3/ -p ${vetsaid} -s ses-03 -c ~/netshare/M/MRI/BIDS/code/bids_conversion/dcm2bids_config_v3.json -o ~/netshare/M/MRI/BIDS/data/
 	
 	echo "Finished converting ${vetsaid}."
 done
